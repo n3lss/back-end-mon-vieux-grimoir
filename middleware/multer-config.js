366fa74ee9399579e26e9ebda1e4016e23b3fb2b
n3lss <nenes93150@gmail.com>
2024-02-11 14:23:36 +0100

    ajout de sharp

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const sharp = require('sharp');
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -6,15 +7,39 @@ const MIME_TYPES = {
   'image/png': 'png'
 };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
-});
+const storage = multer.memoryStorage();
 
-module.exports = multer({storage: storage}).single('image');
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, callback) => {
+    if (MIME_TYPES[file.mimetype]) {
+      callback(null, true);
+    } else {
+      callback(new Error('Format de fichier non pris en charge'), false);
+    }
+  }
+}).single('image');
+
+module.exports = (req, res, next) => {
+  upload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      
+      return res.status(400).json({ error: err.message });
+    } else if (err) {
+      
+      return res.status(400).json({ error: err.message });
+    }
+    const buffer = req.file.buffer;
+    const newFilesName = Date.now();
+    sharp(buffer)
+      .resize( 206 ) 
+      .jpeg({ quality: 70 }) 
+      .toFile(`images/${newFilesName}.jpg`, (error, info) => {
+        if (error) {
+          return res.status(500).json({ error: 'Erreur lors du traitement de l\'image' });
+        }
+        req.file.filename = `${newFilesName}.jpg`
+        next(); 
+      });
+  });
+};
--- routes/book.js
+++ routes/book.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config')
+const multerConfig = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
 router.get('/bestrating', bookCtrl.bestRating)
 router.get('/:id', bookCtrl.getOneBook);
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.post('/', auth, multerConfig, bookCtrl.createBook);
+router.put('/:id', auth, multerConfig, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.post('/:id/rating', auth, bookCtrl.newRatingBook)
 
